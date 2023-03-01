6c0d2dcd993b69d7694c33ce2b7776aa9c05f4ed
Marius K <marius.into@gmail.com>
2023-03-01 14:10:49 +0200

    add unscheduled todolist

--- src/Components/Unscheduled.js
+++ src/Components/Unscheduled.js
@@ -1,7 +1,27 @@
-import React from 'react'
+import { useOutletContext } from "react-router-dom"
+import Header from "./Header"
+import Todolist from "./Todolist"
+import QuickTodo from "./QuickTodo"
 
 export default function Unscheduled() {
+  const [todoist] = useOutletContext()
+  const notSheduled = todoist.tasks.filter( item => !item.due )
   return (
-    <div className='content'>Not scheduled</div>
+    <div className='content'>
+      <Header 
+        title={'Not scheduled'} />
+
+      <Todolist 
+        title={'No due date'} 
+        items={notSheduled} 
+        color={'green'} 
+        url={todoist.url}
+        toggle={(todo)=>todoist.toggle(todo)} 
+        sync={todoist.sync} />
+
+      <QuickTodo 
+        sync={todoist.sync} />
+
+    </div>
   )
 }
