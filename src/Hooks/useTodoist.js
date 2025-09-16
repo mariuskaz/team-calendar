d5c5598aeb28ec9843cb7800222866c059bafed6
Marius K <marius.into@gmail.com>
2025-09-16 13:42:56 +0300

    update initial sync

--- src/Hooks/useTodoist.js
+++ src/Hooks/useTodoist.js
@@ -131,7 +131,6 @@ export default function useTodoist() {
             console.log(note.content);
         }
       });
-      setSynced(true);
     };
 
     const handleIncrementalSync = (data) => {
